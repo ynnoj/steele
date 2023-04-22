bd436eb798b21a7b87082bb1b2e6b3bda35b6449
Jonathan Steele <jonathan@steele.pro>
2023-04-22 11:25:09 +0100

    chore: Install, configure `@tailwindcss/typography`

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,6 +5,7 @@ module.exports = {
     './components/**/*.{js,ts,jsx,tsx}',
     './app/**/*.{js,ts,jsx,tsx}',
   ],
+  plugins: [require('@tailwindcss/typography')],
   theme: {
     extend: {
       backgroundImage: {
